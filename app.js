0711d4011650e6d6a3cd54777b3bce1ecd86dd5d
alias722 <maxchengboy@gmail.com>
2021-02-28 22:04:30 +0800

    add_files_and_bugs

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 //import express : to simply import http method
 const express = require("express")
-
+const fs = require("fs")
 //run app with express and app is in port 
 const app = express()
 const port = 17000
 
+
 app.get("/",function(req,res){
-	res.send("Hello World")
-	console.log("respond")
+	fs.readFile("./static/templates/index.html", function(err,data){
+		if(err){
+			console.log("Error loading html file")
+			throw err
+		}
+		res.send(data)
+		console.log("respond")
+	})
+
 })
 
 app.listen(port,() => console.log('server running on %d',port))
